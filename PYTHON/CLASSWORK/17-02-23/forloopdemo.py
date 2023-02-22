330689317d9323233dc9be398b2ab8ab8392ff22
KBTHANKI18 <kbthanki18@gmail.com>
2023-02-22 15:03:32 +0700

    17-02-23 classwork

---
+++ PYTHON/CLASSWORK/17-02-23/forloopdemo.py
@@ -0,0 +1,29 @@
+"""
+#1st variant:
+for i in range(10):
+    print(i)
+"""
+"""
+#2nd variant:
+for i in range(1,10):
+    print(i)
+"""
+"""
+#3rd variant:
+for i in range(18,47,2):
+    print(i)
+"""
+"""
+#4th variant:
+for i in range(20,0,-1):
+    print(i)
+"""
+"""
+n = int(input("Enter Number : "))
+sum = 0
+for i in range(1,n+1):
+    sum=sum+i
+    n=n-1
+
+print("sum : ",sum)    
+"""
---
+++ PYTHON/CLASSWORK/17-02-23/loopdemo.py
@@ -0,0 +1,28 @@
+"""
+n = int(input("Enter A Number : "))
+
+while n>0:
+    print("fuck off")
+    n = n-1
+"""    
+    
+"""    
+#WAP to print the no. from 1 to 20
+
+n = 1
+while n<=20:
+    print(n)
+    n = n+1
+"""
+"""
+#WAP to print the sum of No.s
+
+n = int(input("Enter N : "))
+sum = 0
+
+while n>0:
+    sum = sum+n
+    n = n-1
+    
+print("sum ",sum)        
+"""
