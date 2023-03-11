af434c309ad3f967a0493dc16ef84ed2e448b813
KBTHANKI18 <kbthanki18@gmail.com>
2023-03-11 15:22:05 +0700

    MODULE-2 PYTHON ASSIGNMENT

---
+++ PYTHON/ASSIGNMENT/MODLUE-2/MODULE-2 Q-14.py
@@ -0,0 +1,3 @@
+Q-14. WHAT ARE NEGATIVE INDEXES AND WHY ARE THEY USED?
+A-14.Negative Indexing is used to in Python to begin slicing from the end of the string i.e. the last.
+     Slicing in Python gets a sub-string from a string.
---
+++ PYTHON/ASSIGNMENT/MODLUE-2/MODULE-2 Q-4.py
@@ -0,0 +1,3 @@
+Q-4.HOW MEMORY IS MANANGED IN PYTHON?
+A-4.Memory management in Python involves a private heap containing all Python objects and data structures.
+    The management of this private heap is ensured internally by the Python memory manager.
